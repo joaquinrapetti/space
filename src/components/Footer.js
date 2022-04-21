f76da816ce3e917a978d59cf3bcd3e4fff754acb
joaquinrapetti <j.rapetti92@gmail.com>
2022-04-21 15:02:06 -0300

    pricing route, heroimage

---
+++ src/components/Footer.js
@@ -0,0 +1,77 @@
+import React from "react";
+import "./Footer.css";
+import {
+  FaFacebook,
+  FaLinkedin,
+  FaMailBulk,
+  FaPhone,
+  FaSearchLocation,
+  FaTwitter,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaSearchLocation
+              size={20}
+              style={{ color: "#fff", marginRight: "2rem" }}
+            />
+            <div>
+              <p>123 Acme St.</p>
+              <h4>Houston, Tx</h4>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              1-800-123-1234
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              trips@galaxy.com
+            </h4>
+          </div>
+        </div>
+        <div className="right">
+          <h4>About the company</h4>
+          <p>
+            "Lorem Ipsum is not simply random text. It has roots in a piece of
+            classical Latin literature from 45 BC, making it over 2000 years
+            old. Richard McClintock, a Latin professor at Hampden-Sydney College
+            in Virginia, looked up one of the more obscure Latin words,
+            consectetur, from a Lorem Ipsum passage, and going through the cites
+            of the word in classical literature, discovered the undoubtable
+            source."
+          </p>
+          <div className="social">
+            <FaFacebook
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaTwitter
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
